5a378f0c075f0aa356038fbdd3b376e94e766100
fashaap <bmpasha23@gmail.com>
2024-09-10 17:13:37 +0700

    update fix

--- src/pages/auth/AxiosInstance.jsx
+++ src/pages/auth/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "https://4n3412vj-8000.asse.devtunnels.ms/",
+  baseURL: "https://mnjkvx93-8000.asse.devtunnels.ms/",
 });
 
 export default AxiosInstance;
